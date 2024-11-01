2bee2d12c980f5e6a8330fc373702373406cee90
Avnish Kumar <avnein4988@gmail.com>
2024-11-01 18:18:51 +0530

    saving the user details in database

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,7 +1,10 @@
+import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import NextAuth, { NextAuthConfig } from "next-auth"
 import github from "next-auth/providers/github"
+import { db } from "./db/db"
 
 export default {
+    adapter:DrizzleAdapter(db),
      providers:[github],
      session:{
         strategy:"jwt"
